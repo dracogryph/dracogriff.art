21ffa15d0f84cce6ce6926ef4023b680df3e92d8
gryphilia <31146498+gryphilia@users.noreply.github.com>
2022-08-12 02:10:17 -0300

    Update home page

--- components/squidfix.js
+++ components/squidfix.js
@@ -1,3 +1,3 @@
 document.querySelector('img[alt="previous site"]').src = "squidring/squidring-left.png";
-document.querySelector('img[alt="squid ring webring"]').src = "squidring/squidring-index.png";
+document.querySelector('img[alt="squid ring webring"]').src = "squidring/squidring-center.png";
 document.querySelector('img[alt="next site"]').src = "squidring/squidring-right.png";
