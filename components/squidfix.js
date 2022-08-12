af5e2dc79bd058b01a84b83019711b273c7d9a61
gryphilia <31146498+gryphilia@users.noreply.github.com>
2022-08-12 01:13:21 -0300

    Update home page

--- components/squidfix.js
+++ components/squidfix.js
@@ -1,3 +1,3 @@
-document.getElementById('squidringprev').getElementsByTagName('img')[0].src = "/squidring-left.png/";
-document.getElementById('squidringindex').getElementsByTagName('img')[0].src = "/squidring-center.png/";
-document.getElementById('squidringnext').getElementsByTagName('img')[0].src = "/squidring-right.png/";
+document.getElementById('squidringprev').getElementsByTagName('img')[0].src = "squidring/squidring-left.png";
+document.getElementById('squidringindex').getElementsByTagName('img')[0].src = "squidring/squidring-center.png";
+document.getElementById('squidringnext').getElementsByTagName('img')[0].src = "squidring/squidring-right.png";
