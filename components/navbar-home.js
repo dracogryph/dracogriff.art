b7f496ab1154d37d69083f921de76f101adfada2
gryphilia <31146498+gryphilia@users.noreply.github.com>
2022-08-11 23:48:36 -0300

    Update home page

---
+++ components/navbar-home.js
@@ -0,0 +1,34 @@
+class Navbar extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		this.innerHTML = `
+    <link rel="stylesheet" href="/styles/navbar-home.css" type="text/css"/>
+
+			<p id="hamburger-helper" onclick="toggleNavbar();"><img src="/icons/protostar.svg" class="hamburger-helper"/></p>
+			<div class="navbar" id="hidden">
+				<ul id="buttons">
+					<a href="#about">About</a>
+					<a href="#projects">Projects</a>
+					<a href="#socials">Socials</a>
+					<a href="#contact">Contact</a>
+					<a href="#commissions">Commissions</a>
+					<a href="#friends">Friends</a>
+				</ul>
+			</div>
+		`;
+	}
+}
+customElements.define('navbar-component', Navbar);
+
+function toggleNavbar() {
+  // if navbar isn't hidden, show nav.
+  if (document.getElementById("hidden") !== null) {
+    document.getElementById("hidden").id = "shown";
+  }
+  // else, hide navbar.
+  else {
+    document.getElementById("shown").id = "hidden";
+  }
+}
