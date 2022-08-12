77cbefba58358e226b2108453e30275050deccea
gryphilia <31146498+gryphilia@users.noreply.github.com>
2022-08-12 04:35:03 -0300

    Update home page

--- components/navbar-home.js
+++ components/navbar-home.js
@@ -9,12 +9,12 @@ class Navbar extends HTMLElement {
 			<div id="topbar" onclick="toggleNavbar();"><img src="/icons/protostar.svg" id="hamburger-helper"/></div>
 			<div class="navbar" id="hidden" onclick="toggleNavbar();">
 				<ul id="buttons">
-					<a href="#about" onclick="scrollto('about');">About</a>
-					<a href="#projects" onclick="scrollto('projects');">Projects</a>
-					<a href="#socials" onclick="scrollto('socials');">Socials</a>
-					<a href="#contact" onclick="scrollto('contact');">Contact</a>
-					<a href="#commissions" onclick="scrollto('commissions');">Commissions</a>
-					<a href="#friends" onclick="scrollto('friends');">Friends</a>
+					<a href="#about">About</a>
+					<a href="#projects">Projects</a>
+					<a href="#socials">Socials</a>
+					<a href="#contact">Contact</a>
+					<a href="#commissions">Commissions</a>
+					<a href="#friends">Friends</a>
 				</ul>
 			</div>
 		`;
@@ -31,9 +31,4 @@ function toggleNavbar() {
   else {
     document.getElementById("shown").id = "hidden";
   }
-}
-
-function scrollto(id) {
-	toggleNavbar();
-	document.getElementById(id).scrollIntoView(true);
 }
