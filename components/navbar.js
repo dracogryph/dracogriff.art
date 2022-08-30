1708fd3bdf99fa4d4e2fca89d5b252cd0317c56b
gryphilia <31146498+gryphilia@users.noreply.github.com>
2022-08-30 13:35:19 -0300

    Cleanup

--- components/navbar.js
+++ components/navbar.js
@@ -2,19 +2,6 @@ class Navbar extends HTMLElement {
 	constructor() {
 		super();
 	}
-	connectedCallback() {
-		this.innerHTML = `
-    <link rel="stylesheet" href="/styles/navbar.css" type="text/css"/>
-
-			<div id="topbar"><img src="/icons/protostar.svg" id="hamburger-helper" onclick="toggleNavbar();"/></div>
-			<div class="navbar" id="hidden" onclick="toggleNavbar();">
-				<ul id="buttons">`
-		+ this.innerHTML+
-		`
-				</ul>
-			</div>
-		`;
-	}
 }
 customElements.define('navbar-component', Navbar);
 
